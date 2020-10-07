812a150b651e313b941f87c5c53f70ae96e84a8f
jonjongao <jonjon.liho@gmail.com>
2020-10-07 14:08:13 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png" />
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+document.addEventListener("DOMContentLoaded", fetchJquery);
+window.setTimeout(fetchJquery, 5000);
+
+var init = false;
+function fetchJquery() {
+  if (init == true) {
+    console.log("is already init");
+    return;
+  }
+  init = true;
+  console.log("init");
+}
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,57 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import 'bootstrap/dist/css/bootstrap.css' // 載入 bootstrap CSS 檔
+import 'bootstrap' // 載入 bootstrap 的 JS 檔
+import jQuery from 'jquery'
+
+Vue.config.productionTip = false
+window.$ = window.jQuery = jQuery
+
+$(document).ready(function () {
+  var v = function () {
+    addVue(restoreSpace);
+  };
+  replaceSpace(v);
+
+  $('#mainContainer > span').each(function () {
+    //Counting each bbsrow
+  });
+});
+
+function replaceSpace(callback) {
+  console.log("replace space");
+  //Replace all space in html with <_> incase of trim by Vue
+  $('#mainContainer > span > div > span > span > span').each(function () {
+    var text = $(this).text();
+    $(this).text(text.replace(/\s/g, "<_>"));
+  });
+
+  callback();
+}
+
+function restoreSpace() {
+  console.log("restore space");
+  //Replace all <_> back to space
+  $('#mainContainer > span > div > span > span > span').each(function () {
+    var text = $(this).text();
+    $(this).text(text.replace(/<_>/g, " "));
+  });
+}
+
+function addVue(callback) {
+  console.log("add vue");
+  /* eslint-disable no-new */
+  new Vue({
+    el: '#app',
+    router,
+    components: {
+      App
+    },
+    template: '<App/>'
+  })
+
+  callback();
+}
