4224eba946afc98949baffca51661f061a0e8b3b
jonjongao <jonjon.liho@gmail.com>
2020-10-07 15:18:08 +0800

    done add routing sample

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png" />
+    <div id="nav">
+      <router-link to="/">Index</router-link>
+      <router-link to="/home">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </div>
     <router-view />
   </div>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,34 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Home from "@/views/Home"
+import VueRouter from 'vue-router'
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
-})
+const routes = [
+  {
+    path: '/',
+    name: 'HelloWorld',
+    component: HelloWorld
+  },
+  {
+    path: '/home',
+    name: "Home",
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component:()=>
+      import("@/views/About")
+  }
+];
+
+const router = new VueRouter({
+  moode: "history",
+  base: process.env.BASE_URL,
+  routes
+});
+
+export default router;
